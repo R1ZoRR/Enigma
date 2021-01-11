da8d7ba0b152ae4016a62d948baf7fe03727ec1a
R1ZoRR <61045820+R1ZoRR@users.noreply.github.com>
2021-01-11 16:42:33 +0300

    Добавил обратную кодировку, нашел и исправил ошибку в дефолтном словаре, окончательно убрал "непонятные" переменные в основновной функции.

--- test.py
+++ test.py
@@ -5,16 +5,23 @@ def summon_enigma_machine():
     В свою очередь машина состоит из нескольких функций.
     """
     print('Введите текст для машины:')
-    initial_text, y, output_text, set_of_keys, i = input(), '', '', '', 1  # тут я присвоил нужные типы обьектам
-    y, set_of_keys = text_ripper(initial_text) # тут я бызвал потрошителя и получил от него "потроха" с набором символов
-    backward_set_of_keys = {} # название говорит само за себя
+    # тут я присвоил нужные типы обьектам
+    initial_text, rewritten_text, output_text, set_of_keys, i = input(), '', '', '', 1
+    # тут я вызвал потрошителя и получил от него "потроха" с набором символов
+    rewritten_text, set_of_keys = text_ripper(initial_text)
+    backward_set_of_keys = {}  # название говорит само за себя
     for i in set_of_keys:
-        backward_set_of_keys[set_of_keys[i]] = i
+        if i != ' ':  # тут пришлось добавить исколючение, т к выполнение останавливалось
+            backward_set_of_keys[int(set_of_keys[i])] = i
     print(backward_set_of_keys)
-    for i in range(0, int(len(y) / 3)):
-        if y[i * 3: i * 3 + 3] != '   ':  # тут я гарантирую себе, что скормлю роторам цифры, а не пробелы
+    for i in range(0, int(len(rewritten_text) / 3)):
+        if rewritten_text[i * 3: i * 3 + 3] != '   ':  # тут я гарантирую себе, что скормлю роторам цифры, а не пробелы
             # тут я вызываю роторы, запихиваю в них кусок текста, возвращаю результат и превращаю его из чисел в буквы
-            output_text += str(rotor_a(int(y[i * 3: i * 3 + 3]), len(set_of_keys) - 1))
+            s = int(rotor_a(int(rewritten_text[i * 3: i * 3 + 3]), len(set_of_keys) - 1)) # добавил для красоты кода
+            if s in backward_set_of_keys:
+                output_text += backward_set_of_keys[s]
+            else:
+                print('error: ',s)
         else:
             output_text += ' '  # три пробела были нужны для кратности
     print(output_text)
@@ -26,10 +33,10 @@ def text_ripper(text, set_of_keys = ''):
     :param text: исходный текст
     :return:
     """
-    default_set_of_keys = {'a': '001', 'b': '002', 'c': '003', 'd': '004', 'e': '005', 'f': '006', 'j': '007',
-                           'h': '008', 'i': '009', 'k': '011', 'l': '012', 'm': '013', 'n': '014',
-                           'o': '015', 'p': '016', 'r': '018', 's': '019', 't': '020', 'u': '021',
-                           'v': '022', 'q': '023', 'x': '024', 'y': '025', 'z': '026', ' ': '   '}
+    default_set_of_keys = {'a': '001', 'b': '002', 'c': '003', 'd': '004', 'e': '005', 'f': '006', 'g': '007', 'h': '008',
+                           'i': '009', 'j': '010', 'k': '011', 'l': '012', 'm': '013', 'n': '014',
+                           'o': '015', 'p': '016', 'q': '017', 'r': '018', 's': '019', 't': '020', 'u': '021',
+                           'v': '022','w': '023', 'x': '024', 'y': '025', 'z': '026', ' ': '   '}
     if set_of_keys == '':
         set_of_keys = default_set_of_keys
     miss_counter, return_text = 0, ''  # тут я присвоил нужные типы обьектам
