4492099f64423c94bfaaa4f13b615e8eea5b14c1
R1ZoRR <61045820+R1ZoRR@users.noreply.github.com>
2021-01-13 00:38:17 +0300

    Подготовил почву для "полноценной" работы. Улучшил качество кода.

--- test.py
+++ test.py
@@ -1,35 +1,40 @@
-def summon_enigma_machine():
+def summon_enigma_machine(initial_text=''):
     """
     Эта функция должна спрашивать параметры, для запуска "машины".
     В свою очередь машина состоит из нескольких функций.
     """
-    print('Введите текст для машины:')
-    # тут я присвоил нужные типы обьектам
-    initial_text, rewritten_text, output_text, set_of_keys, i = input(), '', '', '', 1
-    # тут я вызвал потрошителя и получил от него "потроха" с набором символов
+    # Тут я присвоил нужные типы обьектам
+    rewritten_text, output_text, set_of_keys = '', '', ''
+    print('Укажите параметры для роторов следующим образом: Название ротора(без пробелов)позиция ротора, и т. д.')
+    rotor_poz = input()
+    # Тут я вызвал потрошителя и получил от него "потроха" с набором символов
     rewritten_text, set_of_keys = text_ripper(initial_text)
-    backward_set_of_keys = {}  # название говорит само за себя
+    # Тут я создаю "обратный" словарь. Внимание, если ключ в оригинальном словаре не интуется - программа остановится
+    backward_set_of_keys = {}
     for i in set_of_keys:
         if i != ' ':  # тут пришлось добавить исколючение, т к выполнение останавливалось
             backward_set_of_keys[int(set_of_keys[i])] = i
     print(backward_set_of_keys)
+
     for i in range(0, int(len(rewritten_text) / 3)):
-        if rewritten_text[i * 3: i * 3 + 3] != '   ':  # тут я гарантирую себе, что скормлю роторам цифры, а не пробелы
-            # тут я вызываю роторы, запихиваю в них кусок текста, возвращаю результат и превращаю его из чисел в буквы
-            s = int(rotor_a(int(rewritten_text[i * 3: i * 3 + 3]), len(set_of_keys) - 1))  # добавил для красоты кода
-            if s in backward_set_of_keys:
-                output_text += backward_set_of_keys[s]
+        if rewritten_text[i * 3: i * 3 + 3] != '   ':  # Тут я гарантирую себе, что скормлю роторам цифры, а не пробелы
+            key_id = int(rewritten_text[i * 3: i * 3 + 3])
+            # Тут я вызываю роторы, запихиваю в них кусок текста, возвращаю результат и превращаю его из чисел в буквы
+            if key_id in backward_set_of_keys:
+                output_text += backward_set_of_keys[key_id]
             else:
-                print('error: ', s)
+                print('error: ', key_id)
         else:
-            output_text += ' '  # три пробела были нужны для кратности
+            output_text += ' '  # Три пробела были нужны для кратности
     print(output_text)
 
 
-def text_ripper(text, set_of_keys=''):
+def text_ripper(text_sample, set_of_keys=''):
     """
     Эта функция будет "свежевать" заданый текст, превращать его в цифры и выбрасывать лишнее (чего не будет с словаре
-    :param text: исходный текст
+    :param text_sample: исходный текст
+    :param set_of_keys: Словарь. Нужен для импорта словаря в функцию. Если импорта не происходит берется деф. словарь.
+    Словарь всегда можно заменить, вызвав нужный словарь в качестве второго аргумента
     :return:
     """
     default_set_of_keys = {'a': '001', 'b': '002', 'c': '003', 'd': '004', 'e': '005', 'f': '006', 'g': '007',
@@ -38,30 +43,42 @@ def text_ripper(text, set_of_keys=''):
                            'v': '022', 'w': '023', 'x': '024', 'y': '025', 'z': '026', ' ': '   '}
     if set_of_keys == '':
         set_of_keys = default_set_of_keys
-    miss_counter, return_text = 0, ''  # тут я присвоил нужные типы обьектам
-    for i in text:
-        if i in set_of_keys:  # тут я проверяю каждый символ в исходной строке, на наличие в списке
-            return_text += set_of_keys[i]  # если такой найдется, то он добавляется в "очередь" на обработку
+    miss_counter, return_text = 0, ''  # Тут я присвоил нужные типы обьектам
+    for i in text_sample:
+        if i in set_of_keys:  # Тут я проверяю каждый символ в исходной строке, на наличие в списке
+            return_text += set_of_keys[i]  # Если такой найдется, то он добавляется в "очередь" на обработку
         else:
             miss_counter += 1
     print(return_text, ' ,Miss counter = ', miss_counter)
     return return_text, set_of_keys
 
 
-def rotor_stage(y, set_of_keys):
+def rotor_stage(y, set_of_keys, poz):
     print('Задайте параметры для ротора: ')
 
 
 # Здесь находятся сами роторы
-def rotor_a(key_id, len_of_keys):
+def rotor_a(key_id, len_of_keys, mode='forward'):
     if 1 == (key_id % 4):
-        key_id = key_id + 2
+        if mode == 'forward':
+            key_id = key_id - 1
+        else:
+            key_id = key_id + 2
     elif 2 == (key_id % 4):
-        key_id = key_id - 1
+        if mode == 'forward':
+            key_id = key_id + 1
+        else:
+            key_id = key_id - 2
     elif 3 == (key_id % 4):
-        key_id = key_id + 1
+        if mode == 'forward':
+            key_id = key_id - 2
+        else:
+            key_id = key_id - 1
     elif 0 == (key_id % 4):
-        key_id = key_id - 2
+        if mode == 'forward':
+            key_id = key_id + 2
+        else:
+            key_id = key_id + 1
     if key_id > len_of_keys - 1:
         key_id -= len_of_keys
     elif key_id < 1:
@@ -78,4 +95,6 @@ def rotor_caesar(key_id, len_of_keys, shift):
     return key_id
 
 
-summon_enigma_machine()
+print('Введите текст для машины:')
+text = input()
+summon_enigma_machine(text)
